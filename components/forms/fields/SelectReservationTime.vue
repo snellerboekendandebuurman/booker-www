78474a1b238b633269b13d2822f65f3ea7947958
Joris Jansen <joris97jansen@gmail.com>
2023-12-06 20:32:11 +0100

    Date format for reservation and schedule time

--- components/forms/fields/SelectReservationTime.vue
+++ components/forms/fields/SelectReservationTime.vue
@@ -8,7 +8,7 @@
     <div class="relative">
       <div class="flex justify-between items-center relative">
         <Field name="reservation_time" class="hidden" />
-        <VueDatePicker v-model="date" time-picker-inline />
+        <VueDatePicker v-model="date" time-picker-inline :format="dateFormat"/>
       </div>
       <ErrorMessage
         name="reservation_time"
@@ -26,6 +26,7 @@ import "@vuepic/vue-datepicker/dist/main.css";
 const { value: reservationTimeValue } = useField("reservation_time");
 
 const date = ref(new Date());
+const dateFormat = 'dd/MM/yyyy HH:mm' 
 
 watch(date, (newValue, oldValue) => {
   if (newValue !== oldValue) {
